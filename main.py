ff079fc0d76d8166b739c29c98bd41e98136e72e
OlgaRv <olgavr2959@gmail.com>
2024-04-20 16:31:18 +0300

    Update files

---
+++ main.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect("example.db")
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM users")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+conn.commit()
+
+conn.close()
+
